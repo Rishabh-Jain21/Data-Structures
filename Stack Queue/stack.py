5cd86db5fbd7af69973a369c0609e479b6a970d6
Rishabh <Rishabh9843@gmail.com>
2020-12-13 12:56:56 +0530

    Handled corner cases in stack data structure

--- Stack Queue/stack.py
+++ Stack Queue/stack.py
@@ -9,16 +9,17 @@ class Stack(object):
         return self.items == []
 
     def peep(self):
-        return self.items[-1]
+        if self.items:
+            return self.items[-1]
 
     def pop(self):
-        return self.items.pop()
+        if self.items:
+            return self.items.pop()
 
     def size(self):
         return len(self.items)
 
 
-'''
 s = Stack()
 print(s.isEmpty())
 s.push(1)
@@ -27,4 +28,3 @@ print(s.pop())
 print(s.peep())
 s.push(4)
 print(s.pop())
-'''
