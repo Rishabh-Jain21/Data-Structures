8d312b4fab9b7bdedf0ed1f39655c887972709d9
Rishabh Jain <rishabh9843@gmail.com>
2023-05-28 13:41:03 +0530

    Added recusrion codes

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -23,9 +23,40 @@ def fib_another(n):
 
     return num[n]
 
+def fibo_dp_mem(n):
+    mem = [0, 1]
+    for i in range(2, n + 1):
+        mem[i % 2] = mem[0] + mem[1]
+    return mem[n % 2]
 
+
+
+memo_dict=dict()
+
+def fib_memo(n):
+    if n in memo_dict:
+        return memo_dict[n]
+    if n<=2:
+        return 1
+    memo_dict[n]=fib_memo(n-1)+fib_memo(n-2)
+    return memo_dict[n]
+print(fibonacci(5))
+print(iterative(10))
+
+memo_dict=dict()
+
+def fib_memo_v2(n,dic={}):
+    if n in memo_dict:
+        return memo_dict[n]
+    if n<=2:
+        return 1
+    dic[n]=fib_memo_v2(n-1,dic)+fib_memo_v2(n-2,dic)
+    return dic[n]
 print(fibonacci(5))
 print(iterative(10))
 
 # print(fib_dyn(10))
-print(fib_another(10))
+print(fib_another(15))
+# print(fibo_dp_mem(4))
+print(fib_memo(10))
+print(fib_memo_v2(15))
--- Recursion/permutation.py
+++ Recursion/permutation.py
@@ -12,5 +12,23 @@ def permutate(s):
     return out
 
 
-print(len(permutate('abc')))
+a = []
 
+result = 0
+
+
+def permutate_2(output, string, result):
+    if len(string) == 0:
+        a.append(output)
+        result = result+1
+        return result
+
+    for i in range(len(string)):
+        op_1 = output+string[i]
+        k = string[:i]+string[i+1:]
+        return permutate_2(op_1, k, result)
+
+
+print((permutate('abc')))
+print(permutate_2("", 'abc', result))
+print(a)
